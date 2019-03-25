17d3a78ee320f1872628e6573e125dcb0db43a03
gnovy <thegnovy@gmail.com>
2019-03-25 23:05:21 +0800

    refactor table name; add authCode

---
+++ backend/config.js
@@ -0,0 +1,7 @@
+module.exports.aws = () => {
+    return {
+        tableNames: {
+            orderTable: "orders"
+        }
+    }
+};
--- backend/services/orderService.js
+++ backend/services/orderService.js
@@ -1,22 +1,23 @@
 const uuid = require("uuid");
+const numberUtil = require("../util/numberUtil");
+const config = require("../config");
 
 class OrderService {
     constructor() {
         this._AWS = require("aws-sdk");
         this._DocumentClient = new this._AWS.DynamoDB.DocumentClient();
-
-        this._TableName = "orders";
     }
 
     async create(userId, unitPrice, quantity) {
         let results = await this._DocumentClient.put({
-            TableName : this._TableName,
+            TableName : config.aws().tableNames.orderTable,
             Item: {
                 orderId: uuid.v4(),
                 userId: userId,
                 unitPrice: unitPrice,
                 quantity: quantity,
                 createdTime: new Date().getTime(),
+                authCode: numberUtil.generateRandomNumber(1, 5),
                 status: "created"
             }
         }).promise();
